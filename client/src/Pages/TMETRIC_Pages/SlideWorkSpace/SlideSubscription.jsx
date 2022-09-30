54ca772e43ed9445545bcb0f7d7df793769c8a6a
AdilAhmed976 <adilahmedah976@gmail.com>
2022-09-30 17:03:51 +0530

    subsription-page-added

--- client/src/Pages/TMETRIC_Pages/SlideWorkSpace/SlideSubscription.jsx
+++ client/src/Pages/TMETRIC_Pages/SlideWorkSpace/SlideSubscription.jsx
@@ -2,6 +2,7 @@ import { Box, Flex } from '@chakra-ui/react';
 import React from 'react'
 import { useSelector } from 'react-redux';
 import SideBar from '../../../component/SideBar';
+import { SubscriptionCurrent } from '../../Workspace-subscription/SubscriptionPage/SubscriptionCurrent';
 
 function SlideSubscription() {
     const sideSize = useSelector((state) => state.auth.sideSize);
@@ -16,7 +17,7 @@ function SlideSubscription() {
                 <Box transition={' 0.5s ease-in-out'}
                     ml={sideSize == 'large' ? '180px' : "300px"}
                     border='1px solid red' w='78%' mt='20px' h='auto'>
-                    <h1>Subscription</h1>
+                    <SubscriptionCurrent/>
                 </Box>
 
             </Flex>
