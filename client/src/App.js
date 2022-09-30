d5d1d3bf93295f31019e636e267c1be80ea87700
Rushikesh Parte <96285307+rushi2784@users.noreply.github.com>
2022-09-30 14:14:29 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import SlideClient from './Pages/TMETRIC_Pages/Manage/SlideClient';
+
 import MainRoutes from './Routes/MainRoutes';
 
 function App() {
   return (
     <div className="App">
-      {/* <MainRoutes /> */}.
-      <SlideClient/>
+       <MainRoutes /> 
+
     </div>
   );
 }
