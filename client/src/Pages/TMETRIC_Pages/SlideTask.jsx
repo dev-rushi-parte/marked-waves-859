0e7f83627e2855bfe3d070b547288d232d2991d8
rushi2784 <96285307+rushi2784@users.noreply.github.com>
2022-10-01 15:25:36 +0530

    routes change

--- client/src/Pages/TMETRIC_Pages/SlideTask.jsx
+++ client/src/Pages/TMETRIC_Pages/SlideTask.jsx
@@ -1,5 +1,5 @@
 import { Box, Flex } from '@chakra-ui/react';
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux';
 import SideBar from '../../component/SideBar';
 import FullTask from '../Task/FullTask';
@@ -8,8 +8,8 @@ import TaskHeading from '../Task/TaskHeading';
 
 function SlideTask() {
     const sideSize = useSelector((state) => state.auth.sideSize);
-    const [state , setState] = useState(true);
-    const [taskD , setTaskD] = useState('');
+    const [state, setState] = useState(true);
+    const [taskD, setTaskD] = useState('');
 
     console.log(sideSize, "in Timer")
 
@@ -20,16 +20,16 @@ function SlideTask() {
             </Box>
 
             <Box transition={' 0.5s ease-in-out'} ml={sideSize == 'large' ? '180px' : "310px"} border='1px solid red' w='80%' h='2000px'>
-            <TaskHeading setState={setState } state={state}/>
-            {
-                state ?  <FullTask setState={setState } state={state} setTaskD={setTaskD}/> : <Task taskD={taskD}   setState={setState } state={state}/>
-            }
-            <Box transition={' 0.5s ease-in-out'}
-                ml={sideSize == 'large' ? '180px' : "300px"}
-                border='1px solid red' w='78%' mt='20px' h='auto'>
-                <Task />
+                <TaskHeading setState={setState} state={state} />
+                {
+                    state ? <FullTask setState={setState} state={state} setTaskD={setTaskD} /> : <Task taskD={taskD} setState={setState} state={state} />
+                }
+                <Box transition={' 0.5s ease-in-out'}
+                    ml={sideSize == 'large' ? '180px' : "300px"}
+                    border='1px solid red' w='78%' mt='20px' h='auto'>
+                    <Task />
+                </Box>
             </Box>
-
         </Flex>
     )
 }
