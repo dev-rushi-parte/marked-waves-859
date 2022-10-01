803531deb6e107238c6e76ddf058fcf55b79e229
rushi2784 <96285307+rushi2784@users.noreply.github.com>
2022-10-01 19:45:13 +0530

    donE

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 
-import RoleAuthrization from './component/RoleAuthrization/RoleAuthrization';
+
 import MainRoutes from './component/Routes/MainRoutes';
-import SideBar from './component/SideBar';
+
 
 
 function App() {
   return (
     <div className="App">
-      {/* <SideBar /> */}
+
       <MainRoutes />
 
-      {/* <RoleAuthrization /> */}
+
     </div>
   );
 }
